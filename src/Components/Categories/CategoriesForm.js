c78084c4496ce2883040d9d2f803efe605d4af28
GabrielVodopivec <vodopivecgabriel@gmail.com>
2022-10-27 23:28:53 -0300

    Funcionalidades básicas implementadas, falta hacerlo reutilizable

--- src/Components/Categories/CategoriesForm.js
+++ src/Components/Categories/CategoriesForm.js
@@ -3,7 +3,11 @@ import React from 'react';
 import { useFormik } from 'formik';
 import * as Yup from 'yup';
 
+import { CKEditor } from '@ckeditor/ckeditor5-react';
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+
 import '../FormStyles.css';
+import './categoriesForm.css';
 
 const CategoriesForm = () => {
 
@@ -13,11 +17,13 @@ const CategoriesForm = () => {
         image: ''
     }
 
+    const jpgRegExp = /\.(jpe?g)$/i;
+
     const validationSchema = () =>
         Yup.object().shape({
-            name: Yup.string().required(),
+            name: Yup.string().min(4).required(),
             description: Yup.string().required(),
-            image: Yup.string().required()
+            image: Yup.string().matches(jpgRegExp, { message: 'image must be .jpg file', excludeEmptyString: true }).required()
         })
 
     const onSubmit = () => {
@@ -34,47 +40,62 @@ const CategoriesForm = () => {
         handleSubmit,
         handleChange,
         handleBlur,
-        touched,
+        setFieldValue,
+        setFieldTouched,
         values,
         values: { name, description, image },
+        touched: { name: touchedName, description: touchedDescription, image: touchedImage },
         errors: { name: errorName, description: errorDescription, image: errorImage }
     } = formik;
 
 
     return (
-        <form className="form-container" onSubmit={handleSubmit}>
-            <input
-                className="input-field"
-                type="text"
-                name="name"
-                value={name}
-                onBlur={handleBlur}
-                onChange={handleChange}
-                placeholder="Title"
-            />
-            {errorName && touched && <span>{errorName}</span>}
-            <input
-                className="input-field"
-                type="text"
-                name="description"
-                value={description}
-                onBlur={handleBlur}
-                onChange={handleChange}
-                placeholder="Write some description"
-            />
-            {errorDescription && touched && <span>{errorDescription}</span>}
-            <input
-                className="input-field"
-                type="text"
-                name="image"
-                value={image}
-                onBlur={handleBlur}
-                onChange={handleChange}
-                placeholder="Add an image"
-            />
-            {errorImage && touched && <span>{errorImage}</span>}
-            <button className="submit-btn" type="submit">Send</button>
-        </form>
+        <div>
+            <h1 className='categoriesForm-Title'>Categories Form</h1>
+            <form className="form-container" onSubmit={handleSubmit}>
+                <input
+                    className="input-field"
+                    type="text"
+                    name="name"
+                    value={name}
+                    onBlur={handleBlur}
+                    onChange={handleChange}
+                    placeholder="Title"
+                />
+                <div className='categoriesForm-errorContainer'>
+                    {errorName && touchedName && <span>{errorName}</span>}
+                </div>
+
+                <CKEditor
+                    editor={ClassicEditor}
+                    data={description}
+                    onChange={(event, editor) => {
+                        const data = editor.getData();
+                        setFieldValue('description', data)
+                    }}
+                    onBlur={() => {
+                        setFieldTouched('description', 'description is a required field')
+                    }}
+                />
+
+                <div className='categoriesForm-errorContainer'>
+                    {errorDescription && touchedDescription && <span>{errorDescription}</span>}
+                </div>
+                <input
+                    className="input-field"
+                    type="file"
+                    name="image"
+                    value={image}
+                    onBlur={handleBlur}
+                    onChange={handleChange}
+                    placeholder="Add an image"
+                />
+                <div className='categoriesForm-errorContainer'>
+                    {errorImage && touchedImage && <span>{errorImage}</span>}
+                </div>
+                <button className="submit-btn" type="submit">Send</button>
+            </form>
+        </div>
     );
 }
 

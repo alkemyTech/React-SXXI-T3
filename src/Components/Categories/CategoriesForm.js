b9140ca04d2b8ce46049f7461284f65e92862390
GabrielVodopivec <vodopivecgabriel@gmail.com>
2022-10-29 23:18:22 -0300

    Correciones en traducciones y estilos

--- src/Components/Categories/CategoriesForm.js
+++ src/Components/Categories/CategoriesForm.js
@@ -8,7 +8,6 @@ import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 
 import { onSubmitService } from '../../Services/categoryFormServices';
 
-import '../FormStyles.css';
 import './categoriesForm.css';
 
 const CategoriesForm = ({ category }) => {
@@ -34,14 +33,14 @@ const CategoriesForm = ({ category }) => {
     const validationSchema = () =>
         Yup.object().shape({
             name: Yup
-            .string()
-            .min(4, 'El nombre debe tener 4 caracteres como mínimo').required(`Titulo ${requiredMessage}`),
-            description: Yup.string().required(requiredMessage),
+                .string()
+                .min(4, 'El nombre debe tener 4 caracteres como mínimo').required(`Titulo ${requiredMessage}`),
+            description: Yup.string().required(`La Descripción ${requiredMessage}`),
             image: Yup.string().matches(
                 jpgRegExp, {
                 message: 'La imagen debe se un archivo .jpg o .png',
                 excludeEmptyString: true
-            }).required(requiredMessage)
+            }).required(`La imagen ${requiredMessage}`)
         });
 
     const onSubmit = () => {
@@ -72,48 +71,68 @@ const CategoriesForm = ({ category }) => {
     } = formik;
 
     return (
-        <div>
+        <div className='categoriesContainer'>
             <h1 className='categoriesForm-Title'>Formulario de Categorias</h1>
             <form className="form-container" onSubmit={handleSubmit}>
-                <input
-                    className="input-field"
-                    type="text"
-                    name="name"
-                    value={name}
-                    onBlur={handleBlur}
-                    onChange={handleChange}
-                    placeholder="Titulo"
-                />
-                <div className='categoriesForm-errorContainer'>
-                    {errorName && touchedName && <span>{errorName}</span>}
+                <div className='input-label-contariner'>
+                    <label
+                        htmlFor='inputTitle'
+                    >
+                        Titulo
+                    </label>
+                    <input
+                        id='inputTitle'
+                        className="input-field"
+                        type="text"
+                        name="name"
+                        value={name}
+                        onBlur={handleBlur}
+                        onChange={handleChange}
+                        placeholder="Escriba el título de la categoría"
+                    />
+                    <div className='categoriesForm-errorContainer'>
+                        {errorName && touchedName && <span>{errorName}</span>}
+                    </div>
                 </div>
-
-                <CKEditor
-                    editor={ClassicEditor}
-                    data={description}
-                    onChange={(event, editor) => {
-                        const data = editor.getData();
-                        setFieldValue('description', data)
-                    }}
-                    onBlur={() => {
-                        setFieldTouched('description', 'description is a required field')
-                    }}
-                />
-
-                <div className='categoriesForm-errorContainer'>
-                    {errorDescription && touchedDescription && <span>{errorDescription}</span>}
+                <div className='input-label-contariner'>
+                    <label>
+                        Descripcion
+                    </label>
+                    <CKEditor
+                    className='border-rounded'
+                        editor={ClassicEditor}
+                        data={description}
+                        onChange={(event, editor) => {
+                            const data = editor.getData();
+                            setFieldValue('description', data)
+                        }}
+                        onBlur={() => {
+                            setFieldTouched('description')
+                        }}
+                    />
+                    <div className='categoriesForm-errorContainer'>
+                        {errorDescription && touchedDescription && <span>{errorDescription}</span>}
+                    </div>
                 </div>
-                <input
-                    className="input-field"
-                    type="file"
-                    name="image"
-                    value={image}
-                    onBlur={handleBlur}
-                    onChange={handleChange}
-                    placeholder="Agregar una imagen"
-                />
-                <div className='categoriesForm-errorContainer'>
-                    {errorImage && touchedImage && <span>{errorImage}</span>}
+                <div className='input-label-contariner'>
+                    <label
+                        htmlFor='inputImage'
+                    >
+                        Imagen
+                    </label>
+                    <input
+                        id='inputImage'
+                        className="input-field"
+                        type="file"
+                        name="image"
+                        value={image}
+                        onBlur={handleBlur}
+                        onChange={handleChange}
+                        placeholder="Agregar una imagen"
+                    />
+                    <div className='categoriesForm-errorContainer'>
+                        {errorImage && touchedImage && <span>{errorImage}</span>}
+                    </div>
                 </div>
                 <button className="submit-btn" type="submit">Enviar</button>
             </form>

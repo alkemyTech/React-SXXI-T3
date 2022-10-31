1cc38dddde8bd3fbb42890d4460227b5915c3470
Carlos Esposito <elcharlybeto@hotmail.com>
2022-10-31 09:10:27 -0300

    agrego formulario login y assets

--- src/Components/Auth/LoginForm.js
+++ src/Components/Auth/LoginForm.js
@@ -1,33 +1,90 @@
-import React, { useState } from 'react';
-import '../FormStyles.css';
+import React from "react";
+import { useFormik } from "formik";
+import { Link } from "react-router-dom";
+import * as yup from "yup";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "../FormStyles.css";
+import "./Auth.css";
 
-const LoginForm = () => {
-    const [initialValues, setInitialValues] = useState({
-        email: '',
-        password: ''
-    });
+const LoginForm = ({desktop}) => {
+  const initialValues = {
+    email: "",
+    password: "",
+  };
 
-    const handleChange = (e) => {
-        if(e.target.name === 'email'){
-            setInitialValues({...initialValues, email: e.target.value})
-        } if(e.target.name === 'password'){
-            setInitialValues({...initialValues, password: e.target.value})
-        }
-    }
-    
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        console.log(initialValues);
-        localStorage.setItem('token', 'tokenValueExample')
-    }
+  const validationSchema = yup.object().shape({
+    email: yup
+      .string()
+      .email("El email debe tener un formato válido")
+      .required("El email es obligatorio"),
+    password: yup
+      .string()
+      .min(6, "La contraseña debe tener una longitud mínima de 6 caracteres")
+      .matches(
+        /^(?=.*?[A-Za-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{6,}$/,
+        "La contraseña debe contener al menos un número, una letra y un símbolo (#?!@$%^&*-)"
+      )
+      .required("La contraseña es obligatoria"),
+  });
 
-    return (
-        <form className="form-container" onSubmit={handleSubmit}>
-            <input className="input-field" type="text" name="email" value={initialValues.name} onChange={handleChange} placeholder="Enter email"></input>
-            <input className="input-field" type="text" name="password" value={initialValues.password} onChange={handleChange} placeholder="Enter password"></input>
-            <button className="submit-btn" type="submit">Log In</button>
-        </form>
-    );
-}
- 
+  const onSubmit = ({ email, password }) => {
+    const user = {
+      email,
+      password,
+    };
+    console.log(user);
+  };
+
+  const formik = useFormik({ initialValues, validationSchema, onSubmit });
+
+  const { handleChange, handleBlur, handleSubmit, errors, touched, values } =
+    formik;
+
+  return (
+    <>
+      <form className="form-container" onSubmit={handleSubmit}>
+        {desktop && <>
+        <div className="title">Bienvenido</div>
+        <div className="subtitle">Inicia sesión en tu cuenta!</div>
+        </>}
+        <input
+          className="input-field"
+          type="email"
+          name="email"
+          value={values.email}
+          onChange={handleChange}
+          onBlur={handleBlur}
+          placeholder="Correo electrónico"
+          autoComplete="off"
+        ></input>
+        {errors.email && touched.email && (
+          <div className="error-msg">{errors.email}</div>
+        )}
+        <input
+          className="input-field"
+          type="password"
+          name="password"
+          value={values.password}
+          onChange={handleChange}
+          onBlur={handleBlur}
+          placeholder="Contraseña"
+          autoComplete="off"
+        ></input>
+        {errors.password && touched.password && (
+          <div className="error-msg">{errors.password}</div>
+        )}
+        <button className="submit-btn" type="submit">
+          Inicia sesión
+        </button>
+        <div className="suggestion">
+          No tienes una cuenta?{" "}
+          <Link className="link" to={"/register"}>
+            Regístrate
+          </Link>
+        </div>
+      </form>
+    </>
+  );
+};
+
 export default LoginForm;

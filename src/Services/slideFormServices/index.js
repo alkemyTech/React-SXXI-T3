4844ee43dee3764751bb596d0216fa3095fb74ed
Jose Luis Panero Grando <57608370+junx2000@users.noreply.github.com>
2022-11-01 17:44:05 -0300

    Create index.js

---
+++ src/Services/slideFormServices/index.js
@@ -0,0 +1,69 @@
+import { apiONG } from '../apiONG';
+import Swal from 'sweetalert2';
+
+export const onSubmitService = (slide, name, description, resetForm, setSubmitting) => {
+    if (slide) {
+
+        apiONG
+            .put(`/slides/${slide.id}`, {
+                name,
+                description
+            })
+            .then((response) => {
+                const { data: { message } } = response;
+
+                return Swal.fire({
+                    title: message,
+                    icon: 'success',
+                    timer: 3000
+                })
+            })
+            .catch((error) => {
+                const errorMessage =
+                    error?.response?.data?.message
+                    || error.message;
+
+                Swal.fire({
+                    title: errorMessage,
+                    icon: 'error',
+                    timer: 5000
+                })
+            })
+            .finally(() => {
+                setSubmitting(false)
+            })
+
+    } else {
+
+        apiONG
+            .post(`/slides`, {
+                name,
+                description
+            })
+            .then((response) => {
+                const { data: { message } } = response;
+
+                Swal.fire({
+                    title: message,
+                    icon: 'success',
+                    timer: 3000
+                })
+                return resetForm();
+            })
+            .catch((error) => {
+                const errorMessage =
+                    error?.response?.data?.message
+                        ? `Ya existe una Slide con ese nombre`
+                        : error.message;
+
+                Swal.fire({
+                    title: errorMessage,
+                    icon: 'error',
+                    timer: 5000
+                })
+            })
+            .finally(() => {
+                setSubmitting(false)
+            })
+    }
+}

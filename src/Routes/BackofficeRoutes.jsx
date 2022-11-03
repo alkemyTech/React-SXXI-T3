a3aa21411b79fa6986adb1f64c6c88a6990fbfad
GabrielVodopivec <vodopivecgabriel@gmail.com>
2022-11-02 22:00:28 -0300

    Componente agregado a la ruta correspondiente del backoffice

--- src/Routes/BackofficeRoutes.jsx
+++ src/Routes/BackofficeRoutes.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import {Navigate, Route, Routes} from "react-router-dom";
+import CategoriesForm from "../Components/Categories/CategoriesForm";
+import MembersForm from "../Components/Members/MembersForm.jsx";
 
 const BackofficeRoutes = () => {
     return (
@@ -25,12 +27,12 @@ const BackofficeRoutes = () => {
                 <Route path="editar/:id" element={<></>}/>
             </Route>
             <Route path="miembros">
-                <Route path="crear" element={<></>}/>
-                <Route path="editar/:id" element={<></>}/>
+                <Route path="crear" element={<MembersForm />}/>
+                <Route path="editar/:id" element={<MembersForm />}/>
             </Route>
             <Route path="categorias">
-                <Route path="crear" element={<></>}/>
-                <Route path="editar/:id" element={<></>}/>
+                <Route path="crear" element={<CategoriesForm />}/>
+                <Route path="editar/:id" element={<CategoriesForm />}/>
             </Route>
             <Route path="novedades">
                 <Route path="crear" element={<></>}/>

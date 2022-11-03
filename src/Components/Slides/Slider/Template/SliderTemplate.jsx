558893ed564fc7cc10b6a72dd8011e76d4cafba5
gperrz <gperez01.dev@gmail.com>
2022-11-03 13:20:45 -0300

    Saco height definido, cambio object fit a cover

--- src/Components/Slides/Slider/Template/SliderTemplate.jsx
+++ src/Components/Slides/Slider/Template/SliderTemplate.jsx
@@ -4,11 +4,6 @@ const SliderTemplate = (props) => {
     const htmlContent = {__html: props.description}
     return (
         <>
-            {/*<img src={props.image} alt={props.name} className="template-image"/>*/}
-            {/*<div className="template-content">*/}
-            {/*    <h3 className="template-title">{props.name}</h3>*/}
-            {/*    <div dangerouslySetInnerHTML={htmlContent} className="template-text"/>*/}
-            {/*</div>*/}
             <img src={props.image} alt={props.name} className={style.image}/>
             <div className={style.content}>
                 <h3 className={style.title}>{props.name}</h3>

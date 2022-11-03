2cdf6ef45572fca0034db0432248586554182f52
Jose Luis Panero Grando <57608370+junx2000@users.noreply.github.com>
2022-11-03 02:39:00 -0300

    Create userFormServices.js

---
+++ src/Services/userFormServices.js
@@ -0,0 +1,76 @@
+import { apiONG } from './apiONG';
+import Swal from 'sweetalert2';
+
+export const onSubmitService = (id, name, email, password, profilePhoto, role, resetForm, setSubmitting) => {
+    if (id) {
+
+
+        apiONG
+            .put(`/users/${id}`, {
+                name, 
+                email, 
+                password, 
+                profilePhoto, 
+                role
+            })
+            .then((response) => {
+                const { data: { message } } = response;
+
+                return Swal.fire({
+                    title: message,
+                    icon: 'success',
+                    timer: 3000
+                })
+            })
+            .catch((error) => {
+                const errorMessage =
+                    error?.response?.data?.message
+                    || error.message;
+
+                Swal.fire({
+                    title: errorMessage,
+                    icon: 'error',
+                    timer: 5000
+                })
+            })
+            .finally(() => {
+                setSubmitting(false)
+            })
+
+    } else {
+
+        apiONG
+            .post(`/users`, {
+                name,
+                email,
+                password,
+                profilePhoto,
+                role,
+            })
+            .then((response) => {
+                const { data: { message } } = response;
+
+                Swal.fire({
+                    title: message,
+                    icon: 'success',
+                    timer: 3000
+                })
+                return resetForm();
+            })
+            .catch((error) => {
+                const errorMessage =
+                    error?.response?.data?.message
+                        ? `Ya existe una categoría con ese nombre`
+                        : error.message;
+
+                Swal.fire({
+                    title: errorMessage,
+                    icon: 'error',
+                    timer: 5000
+                })
+            })
+            .finally(() => {
+                setSubmitting(false)
+            })
+    }
+}

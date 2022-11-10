7ea6efd0ca2a4de5a214a3e04913cb2310b58fdb
Jose Luis Panero Grando <57608370+junx2000@users.noreply.github.com>
2022-11-10 02:03:12 -0300

    use mock, and update styles

--- src/Components/Activities/ActivitiesList.jsx
+++ src/Components/Activities/ActivitiesList.jsx
@@ -7,20 +7,22 @@ import Title from '../Title/Title';
 import { NewsCard } from './NewsCard';
 
 const ActivitiesList = () => {
-    const [activities, setActivities] = useState([]);
-
-    //useEffect(() => {
-     //   apiONG.get('/activities')
-    //    .then((response) => setActivities(response.data.data))
-        // .catch((error) => ) SCREEN DE ERROR O REDIRECCIONAR AL HOME 
-    //}, []);
+    const mock = [
+        {id:1, name: 'Actividad 1', description:'Descripcion de la actividad 1, Descripcion de la actividad 1, Descripcion de la actividad 1,Descripcion de la actividad 1Descripcion de la actividad 1Descripcion de la actividad 1Descripcion de la actividad 1Descripcion de la actividad 1', image: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.RiNmH5AIoZwVGvB835DNuAHaE8%26pid%3DApi&f=1&ipt=aef8fd17edcb70c26f62c326f727ad27bff17766c7f8b21d39830f8f3859dc5e&ipo=images'},
+        {id:2, name: 'Actividad 2', description:'Descripcion de la actividad 2', image: ''},
+        {id:3, name: 'Actividad 3', description:'Descripcion de la actividad 3', image: ''},
+        {id:4, name: 'Actividad 4', description:'Descripcion de la actividad 4', image: ''},
+        {id:5, name: 'Actividad 5', description:'Descripcion de la actividad 5', image: ''},
+        {id:6, name: 'Actividad 6', description:'Descripcion de la actividad 6', image: ''},
+        {id:7, name: 'Actividad 7', description:'Descripcion de la actividad 7', image: ''},
+    ];
 
     return (
         <div className="container">
           <Title title="Actividades" />
           <ul className="list-container mt-3 row">
-            {activities.length > 0 ? (
-              activities.map((element) => {
+            {mock.length > 0 ? (
+              mock.map((element) => {
                 return (
                   <NewsCard
                     id={element.id}

910bf61d8854b6b8ff82c8e8001aeedd5748c604
Jose Luis Panero Grando <57608370+junx2000@users.noreply.github.com>
2022-11-10 00:31:58 -0300

    rename .js to .jsx

--- src/Components/Activities/ActivitiesDetail.jsx
+++ src/Components/Activities/ActivitiesDetail.jsx
@@ -1,8 +1,8 @@
 import {useEffect, useState} from "react";
 import {useParams} from "react-router-dom";
 
-import DetailCard from "../../DetailCard/DetailCard";
-import Title from "../../Title/Title";
+import DetailCard from "../DetailCard/DetailCard";
+import Title from "../Title/Title";
 
 
 const ActivitiesDetail = () => {

b401939b45cf5d729248235cdf04ba18dca3e762
Carlos Esposito <elcharlybeto@hotmail.com>
2022-11-15 09:20:07 -0300

    Listado de Novedades en Bakoffice-Dashboard

--- src/Components/Dashboard/FeatureCard.jsx
+++ src/Components/Dashboard/FeatureCard.jsx
@@ -1,8 +1,13 @@
 import React from "react";
 import "./FeatureCard.css";
 import Button from "../Button/Button";
+import { useNavigate } from "react-router-dom";
+
 
 export const FeatureCard = ({ name, logo, link }) => {
+  
+  const navigate=useNavigate();
+
   return (
     <div className="feature-container shadow">
       <div className="feature-name">{name}</div>
@@ -10,7 +15,7 @@ export const FeatureCard = ({ name, logo, link }) => {
         <img src={logo} alt={name} />
       </div>
       <div className="feature-link">
-        <Button label="Ir" variant="primary" className="feature-btn" />
+        <Button onClick={()=>navigate(link)} label="Ir" variant="primary" className="feature-btn" />
       </div>
     </div>
   );
---
+++ src/utils/DateParser.js
@@ -0,0 +1,10 @@
+export const DateParser = (date) => {
+    const tempDate = new Date(Date.parse(date));
+    return tempDate.toLocaleString('es-AR', {
+        year: 'numeric',
+        month: 'numeric',
+        day: 'numeric',
+        hour: 'numeric',
+        minute: 'numeric'
+    });
+}

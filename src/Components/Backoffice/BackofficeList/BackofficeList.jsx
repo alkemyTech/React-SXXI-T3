3adfde0d538faf2f984b12fa9875f5f0bf01bdef
Jose Luis Panero Grando <57608370+junx2000@users.noreply.github.com>
2022-11-16 17:34:20 -0300

    Squashed commit of the following:
    commit 442ddb5ea4a7e4579036f075ed409999a8d43cd0
    Author: Carlos Esposito <elcharlybeto@hotmail.com>
    Date:   Tue Nov 15 12:17:28 2022 -0300
    
        Agrego visualización de imágenes desde el listado
    
    commit b401939b45cf5d729248235cdf04ba18dca3e762
    Author: Carlos Esposito <elcharlybeto@hotmail.com>
    Date:   Tue Nov 15 09:20:07 2022 -0300
    
        Listado de Novedades en Bakoffice-Dashboard

---
+++ src/Components/Backoffice/BackofficeList/BackofficeList.jsx
@@ -0,0 +1,34 @@
+import {Link} from "react-router-dom";
+
+import Table from "../../Table/Table";
+
+import './BackofficeList.css'
+
+const BackofficeList = ({
+                            title,
+                            createButonLabel="",
+                            createButonVariant = "primary",
+                            tableClassName = "",
+                            tableData = [],
+                            tableHeader = [],
+                            tableNames = [],
+                            deleteFunction
+                        }) => {
+
+
+    return (
+        <>
+            <div className="container backoffice-header">
+                <h1>{title}</h1>
+                <Link to={"crear"} className={`button ${createButonVariant}`}>{`Crear ${createButonLabel.toLowerCase()}`}</Link>
+            </div>
+            <div className="backoffice-table-container">
+                <Table className={tableClassName} title={title} tableData={tableData} tableNames={tableNames} 
+                       tableHeader={tableHeader} handleDelete={deleteFunction}/>
+            </div>
+        </>
+    )
+
+}
+
+export default BackofficeList;

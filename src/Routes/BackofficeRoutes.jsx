dc7073d18b901076a5c4a5455b1e62cfe9440c87
GabrielVodopivec <vodopivecgabriel@gmail.com>
2022-11-18 05:05:28 -0300

    Componente Header de backoffice agregado al router

--- src/Routes/BackofficeRoutes.jsx
+++ src/Routes/BackofficeRoutes.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {Navigate, Route, Routes} from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import UsersForm from "../Components/Users/UsersForm";
 import CategoriesForm from "../Components/Categories/CategoriesForm";
 import MembersForm from "../Components/Members/MembersForm";
@@ -13,53 +13,57 @@ import ProjectsForm from "../Components/Projects/ProjectsForm";
 import OrganizationScreen from "../Components/Organization/OrganizationScreen";
 import OrganizationForm from "../Components/Organization/OrganizationForm";
 import { NewsList } from "../Components/Backoffice/NewsList";
+import HeaderBackoffice from "../Components/HeaderBackoffice";
 
 
 
 const BackofficeRoutes = () => {
     return (
-        <Routes>
-            <Route index element={<Dashboard/>}/>
-            <Route path="home" element={<HomeForm/>}/>
-            <Route path="organizacion">
-                <Route index element={<OrganizationScreen />} />
-                <Route path="editar" element={<OrganizationForm />}/>
-            </Route>
-            <Route path="slides">
-                <Route path="crear" element={<SlidesForm/>}/>
-                <Route path="editar/:id" element={<SlidesForm/>}/>
-            </Route>
-            <Route path="proyectos">
-                <Route path="crear" element={<ProjectsForm/>}/>
-                <Route path="editar/:id" element={<ProjectsForm/>}/>
-            </Route>
-            <Route path="testimonios">
-                <Route path="crear" element={<TestimonialsForm/>}/>
-                <Route path="editar/:id" element={<TestimonialsForm/>}/>
-            </Route>
-            <Route path="usuarios">
-                <Route path="crear" element={<UsersForm/>}/>
-                <Route path="editar/:id" element={<UsersForm/>}/>
-            </Route>
-            <Route path="miembros">
-                <Route path="crear" element={<MembersForm />}/>
-                <Route path="editar/:id" element={<MembersForm />}/>
-            </Route>
-            <Route path="categorias">
-                <Route path="crear" element={<CategoriesForm />}/>
-                <Route path="editar/:id" element={<CategoriesForm />}/>
-            </Route>
-            <Route path="novedades">
-                <Route index element={<NewsList/>}/>
-                <Route path="crear" element={<NewsForm/>}/>
-                <Route path="editar/:id" element={<NewsForm/>}/>
-            </Route>
-            <Route path="actividades">
-                <Route path="crear" element={<ActivitiesForm/>}/>
-                <Route path="editar/:id" element={<ActivitiesForm/>}/>
-            </Route>
-            <Route path="*" element={<Navigate to=""/>}/>
-        </Routes>
+        <>
+            <HeaderBackoffice />
+            <Routes>
+                <Route index element={<Dashboard />} />
+                <Route path="home" element={<HomeForm />} />
+                <Route path="organizacion">
+                    <Route index element={<OrganizationScreen />} />
+                    <Route path="editar" element={<OrganizationForm />} />
+                </Route>
+                <Route path="slides">
+                    <Route path="crear" element={<SlidesForm />} />
+                    <Route path="editar/:id" element={<SlidesForm />} />
+                </Route>
+                <Route path="proyectos">
+                    <Route path="crear" element={<ProjectsForm />} />
+                    <Route path="editar/:id" element={<ProjectsForm />} />
+                </Route>
+                <Route path="testimonios">
+                    <Route path="crear" element={<TestimonialsForm />} />
+                    <Route path="editar/:id" element={<TestimonialsForm />} />
+                </Route>
+                <Route path="usuarios">
+                    <Route path="crear" element={<UsersForm />} />
+                    <Route path="editar/:id" element={<UsersForm />} />
+                </Route>
+                <Route path="miembros">
+                    <Route path="crear" element={<MembersForm />} />
+                    <Route path="editar/:id" element={<MembersForm />} />
+                </Route>
+                <Route path="categorias">
+                    <Route path="crear" element={<CategoriesForm />} />
+                    <Route path="editar/:id" element={<CategoriesForm />} />
+                </Route>
+                <Route path="novedades">
+                    <Route index element={<NewsList />} />
+                    <Route path="crear" element={<NewsForm />} />
+                    <Route path="editar/:id" element={<NewsForm />} />
+                </Route>
+                <Route path="actividades">
+                    <Route path="crear" element={<ActivitiesForm />} />
+                    <Route path="editar/:id" element={<ActivitiesForm />} />
+                </Route>
+                <Route path="*" element={<Navigate to="" />} />
+            </Routes>
+        </>
     );
 }
 export default BackofficeRoutes;

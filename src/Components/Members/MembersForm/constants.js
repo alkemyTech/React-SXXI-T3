1dde44421944415a98fd836f8e3bd9c181ef784c
gperrz <116732317+gperrz@users.noreply.github.com>
2022-11-22 12:46:05 -0300

    Update constants.js

--- src/Components/Members/MembersForm/constants.js
+++ src/Components/Members/MembersForm/constants.js
@@ -1,33 +1,62 @@
 import * as Yup from "yup";
-import {
-  imgRegExp,
-  invalidImageFormatMessage,
-  requiredMessage,
-} from "../../../utils/validation/constants";
+import {imgRegExp, invalidImageFormatMessage, requiredMessage} from "../../../utils/validation/constants";
 
 export const validationSchema = Yup.object().shape({
-  name: Yup.string()
-    .min(4, "El nombre debe tener al menos 4 caracteres")
-    .required(requiredMessage + "el nombre"),
-  facebookUrl: Yup.string()
-    .url("Url de Facebook inválida")
-    .required(requiredMessage + "la url de Facebook"),
-  linkedinUrl: Yup.string()
-    .url("Url de LinkedIn inválida")
-    .required(requiredMessage + "la url de LinkedIn"),
-  description: Yup.string().required(requiredMessage + "la descripción"),
-  image: Yup.string()
-    .matches(imgRegExp, {
-      message: invalidImageFormatMessage,
-      excludeEmptyString: true,
+        name: Yup.string().min(4, 'El nombre debe tener al menos 4 caracteres')
+            .required(requiredMessage + "el nombre"),
+        facebookUrl: Yup.string().url("Url de Facebook inválida")
+            .required(requiredMessage + "la url de Facebook"),
+        linkedinUrl: Yup.string().url("Url de LinkedIn inválida")
+            .required(requiredMessage + "la url de LinkedIn"),
+        description: Yup.string()
+            .required(requiredMessage + "la descripción"),
+    image: Yup
+        .string()
+        .matches(imgRegExp, {
+            message: invalidImageFormatMessage,
+            excludeEmptyString: true,
+        })
+        .required(requiredMessage + "la imagen")
     })
-    .required(requiredMessage + "la imagen"),
-});
+
+const validations = {
+    name: Yup.string().min(4, 'El nombre debe tener al menos 4 caracteres')
+        .required(requiredMessage + "el nombre"),
+    facebookUrl: Yup.string().url("Url de Facebook inválida")
+        .required(requiredMessage + "la url de Facebook"),
+    linkedinUrl: Yup.string().url("Url de LinkedIn inválida")
+        .required(requiredMessage + "la url de LinkedIn"),
+    description: Yup.string()
+        .required(requiredMessage + "la descripción"),
+}
+
+export const createValidationSchema = Yup.object().shape({
+    ...validations,
+    image: Yup
+        .string()
+        .matches(imgRegExp, {
+            message: invalidImageFormatMessage,
+            excludeEmptyString: true,
+        })
+        .required(requiredMessage + "la imagen")
+})
+
+export const editValidationSchema = Yup.object().shape({
+    ...validations,
+    image: Yup
+        .string()
+        .matches(imgRegExp, {
+            message: invalidImageFormatMessage,
+            excludeEmptyString: true,
+        })
+    })
+
+
 
 export const initialValues = {
-  name: "",
-  description: "",
-  image: "",
-  facebookUrl: "",
-  linkedinUrl: "",
-};
+    name: '',
+    description: '',
+    image: '',
+    facebookUrl: '',
+    linkedinUrl: ''
+}

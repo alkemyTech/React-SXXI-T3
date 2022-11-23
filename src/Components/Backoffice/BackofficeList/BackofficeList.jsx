fef0f870fa98a60f124dcbccfd062cb60ebf4d58
GabrielVodopivec <vodopivecgabriel@gmail.com>
2022-11-23 13:17:29 -0300

    Implementación del input de búsqueda en el componente BackofficeList

--- src/Components/Backoffice/BackofficeList/BackofficeList.jsx
+++ src/Components/Backoffice/BackofficeList/BackofficeList.jsx
@@ -1,31 +1,37 @@
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
+import SearchInput from "../../SearchInput";
 
 import Table from "../../Table/Table";
 
 import './BackofficeList.css'
 
 const BackofficeList = ({
-                            title,
-                            createButonLabel="",
-                            createButonVariant = "primary",
-                            tableClassName = "",
-                            tableData = [],
-                            tableHeader = [],
-                            tableNames = [],
-                            deleteFunction,
-                            openImageInNewTab = false
-                        }) => {
-
+    title,
+    createButonLabel = "",
+    createButonVariant = "primary",
+    tableClassName = "",
+    tableData = [],
+    tableHeader = [],
+    tableNames = [],
+    deleteFunction,
+    openImageInNewTab = false,
+    handleChange,
+    handleSubmit
+}) => {
 
     return (
         <>
             <div className="container backoffice-header">
                 <h1>{title}</h1>
                 <Link to={"crear"} className={`button ${createButonVariant}`}>{`Crear ${createButonLabel.toLowerCase()}`}</Link>
+                <SearchInput
+                    handleChange={handleChange}
+                    handleSubmit={handleSubmit}
+                />
             </div>
             <div className="backoffice-table-container">
-                <Table className={tableClassName} title={title} tableData={tableData} tableNames={tableNames} 
-                       tableHeader={tableHeader} handleDelete={deleteFunction} openImageInNewTab={openImageInNewTab}/>
+                <Table className={tableClassName} title={title} tableData={tableData} tableNames={tableNames}
+                    tableHeader={tableHeader} handleDelete={deleteFunction} openImageInNewTab={openImageInNewTab} />
             </div>
         </>
     )

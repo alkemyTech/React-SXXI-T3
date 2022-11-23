0c0c1f4b2c7a33ce2753f72719752158c333e7b2
Carlos Esposito <elcharlybeto@hotmail.com>
2022-11-23 18:02:38 -0300

    Agrego Servicio de Alertas

---
+++ src/Components/Feedback/AlertService.jsx
@@ -0,0 +1,55 @@
+import Swal from "sweetalert2";
+
+const capitalize = (string) => {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+};
+
+export const infoAlert = (title, description, timer, bar) => {
+
+  title = capitalize(title.trim());
+  description = capitalize(description.trim());
+
+  Swal.fire({
+    title: title,
+    html: description,
+    icon: "success",
+    timer,
+    timerProgressBar: bar || false,
+  });
+};
+
+export const errorAlert = (title, description, timer, bar) => {
+
+  title = capitalize(title.trim());
+  description = capitalize(description.trim());
+
+  Swal.fire({
+    title: title,
+    html: description,
+    icon: "error",
+    timer,
+    timerProgressBar: bar || false,
+  });
+};
+
+export const confirmAlert = (question, description, action, cancelled) => {
+
+  question = capitalize(question.trim());
+  description = capitalize(description.trim());
+
+  Swal.fire({
+    icon: "warning",
+    title: question,
+    text: description,
+    showDenyButton: true,
+    showCancelButton: false,
+    confirmButtonText: "Confirmar",
+    denyButtonText: "Cancelar",
+  }).then((result) => {
+    if (result.isConfirmed) {
+      action();
+    } else if (result.isDenied) {
+      cancelled();
+    }
+  });
+};

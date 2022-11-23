d2a63dd7f1b38592731a7e039a4d8f49d2fbca57
GabrielVodopivec <vodopivecgabriel@gmail.com>
2022-11-23 13:11:57 -0300

    Implementación de Hook para controlar las solicitudes

---
+++ src/Components/Backoffice/Hook/index.js
@@ -0,0 +1,32 @@
+import { useState, useEffect } from 'react';
+import Swal from 'sweetalert2';
+import { apiONG } from '../../../Services/apiONG';
+
+export const useBackofficeInfo = (path) => {
+    const [route, setRoute] = useState(path)
+    const [info, setInfo] = useState(null);
+    const [isFetching, setIsFetching] = useState(true);
+
+    useEffect(() => {
+        apiONG
+            .get(`/${route}`)
+            .then(({ data: { data } }) => {
+                setIsFetching(() => (false))
+                setInfo(() => (data))
+            })
+            .catch((error) => {
+                const errorMessage =
+                    error?.response?.data?.message
+                    || error.message;
+                setIsFetching(() => (false))
+                Swal.fire({
+                    title: errorMessage,
+                    icon: 'error',
+                    timer: 5000
+                })
+            })
+    }, [route])
+
+    return [info, isFetching, setRoute]
+}
+

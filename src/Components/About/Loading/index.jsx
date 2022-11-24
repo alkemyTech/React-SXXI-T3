bb25397a0ac5248f22ade121d6bc24bf41821045
Carlos Esposito <elcharlybeto@hotmail.com>
2022-11-24 14:08:08 -0300

    Agrego Spinner

--- src/Components/About/Loading/index.jsx
+++
@@ -1,9 +0,0 @@
-import s from "./loading.module.css";
-
-export default function Loading() {
-  return (
-    <div className={s["loader_container"]}>
-      <div className={s["loader"]}></div>
-    </div>
-  );
-}

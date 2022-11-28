e7ee611566cb831d5e10d8f63782aad81be4feb4
Jose Luis Panero Grando <57608370+junx2000@users.noreply.github.com>
2022-11-27 21:09:04 -0300

    Create apiService.js

---
+++ src/Services/apiService.js
@@ -0,0 +1,73 @@
+import { ApiCore } from "./apiCore";
+
+export const apiNews = new ApiCore({
+    getAll: true,
+    getSingle: true,
+    post: true,
+    put: true,
+    patch: false,
+    delete: true,
+    url: 'news'
+});
+
+export const apiActivity = new ApiCore({
+    getAll: true,
+    getSingle: true,
+    post: true,
+    put: true,
+    patch: false,
+    delete: true,
+    url: 'activities'
+});
+
+export const apiUser = new ApiCore({
+    getAll: true,
+    getSingle: true,
+    post: true,
+    put: true,
+    patch: false,
+    delete: true,
+    url: 'users'
+});
+
+export const apiCategory = new ApiCore({
+    getAll: true,
+    getSingle: true,
+    post: true,
+    put: true,
+    patch: false,
+    delete: true,
+    url: 'categories'
+});
+
+export const apiContact = new ApiCore({
+    getAll: true,
+    getSingle: true,
+    post: true,
+    put: true,
+    patch: false,
+    delete: true,
+    url: 'contacts'
+});
+
+export const apiSlide = new ApiCore({
+    getAll: true,
+    getSingle: true,
+    post: true,
+    put: true,
+    patch: false,
+    delete: true,
+    url: 'slides'
+});
+
+export const apiMember = new ApiCore({
+    getAll: true,
+    getSingle: true,
+    post: true,
+    put: true,
+    patch: false,
+    delete: true,
+    url: 'members'
+});
+
+

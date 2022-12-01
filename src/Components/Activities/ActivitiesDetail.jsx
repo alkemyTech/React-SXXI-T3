1b1f8d7f407125e4cbe49a647ff8148d307ec53e
Carlos Esposito <elcharlybeto@hotmail.com>
2022-12-01 12:22:32 -0300

    User Feedback en Listado y Detalle de Actividades

--- src/Components/Activities/ActivitiesDetail.jsx
+++ src/Components/Activities/ActivitiesDetail.jsx
@@ -1,34 +1,50 @@
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { apiONG } from "../../Services/apiONG";
 import DetailCard from "../Card/DetailCard/DetailCard";
+import { errorAlert } from "../Feedback/AlertService";
+import { Spinner } from "../Feedback/Spinner/Spinner";
 
 import Title from "../Title/Title";
 
 const ActivitiesDetail = () => {
   const { id } = useParams();
   const [activity, setActivity] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
+  const navigate = useNavigate();
 
   useEffect(() => {
-    apiONG.get("/activities/" + id).then((response) => {
-      setActivity(response.data.data);
-    });
+    apiONG
+      .get("/activities/" + id)
+      .then((response) => {
+        setActivity(response.data.data);
+      })
+      .catch((error) => {
+        errorAlert();
+      })
+      .finally(() => setIsLoading(false));
   }, [id]);
 
   return (
     <div>
       <Title title={activity.name} />
-      <DetailCard
-        content={activity.description}
-        date={activity.created_at}
-        color="primary"
-        buttonLabel="Volver a actividades"
-        imageUrl={activity.image}
-        imageAlt={activity.name}
-        onClick={() => {
-          document.location.href = "/actividades";
-        }}
-      />
+      {isLoading ? (
+        <div className="container-fluid mt-5 d-flex justify-content-center align-items-center">
+          <Spinner />
+        </div>
+      ) : (
+        <DetailCard
+          content={activity.description}
+          date={activity.created_at}
+          color="primary"
+          buttonLabel="Volver a actividades"
+          imageUrl={activity.image}
+          imageAlt={activity.name}
+          onClick={() => {
+            navigate("/actividades");
+          }}
+        />
+      )}
     </div>
   );
 };

d044ca599cce1d0cc73f52d834922554db1e50fc
GabrielVodopivec <vodopivecgabriel@gmail.com>
2022-12-02 11:19:47 -0300

    Integración de lógica de manejo de sesiones en vistas y formularios de Login y registro

--- src/Components/Auth/Register.jsx
+++ src/Components/Auth/Register.jsx
@@ -1,12 +1,27 @@
-import "bootstrap/dist/css/bootstrap.min.css";
-import React, { useEffect, useState } from "react";
-import "../FormStyles.css";
-import "./Auth.css";
-import img from "../../assets/images/login.jpg";
+import { useEffect, useState } from "react";
+import { Navigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+
+// Components - Services
 import RegisterForm from "./RegisterForm";
+import { errorAlert } from "../Feedback/AlertService";
+
+// Redux Actions
+import { cleanError, selectAuth } from "../../features/auth/authSlice";
+
+// Assets
+import img from "../../assets/images/login.jpg";
 import logo from "../../assets/images/logo.png";
 
+// Styles
+import "../FormStyles.css";
+import "./Auth.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
 const Register = () => {
+  const dispatch = useDispatch();
+  const { token, error, status } = useSelector(selectAuth);
+
   const [isMobile, setIsMobile] = useState(false);
 
   useEffect(() => {
@@ -21,11 +36,26 @@ const Register = () => {
     };
   }, []);
 
+  useEffect(() => {
+    if (status === 'succeeded' && error) {
+      errorAlert('email o contraseña invalidos');
+      dispatch(cleanError());
+    }
+    if (status === 'failed') {
+      errorAlert('Hay problemas con la red, revisa tu conexion a internet')
+      dispatch(cleanError());
+    }
+  }, [status, error, dispatch])
+
   return (
     <>
       <div className="row flex-sm-row flex-column-reverse">
         <div className="col-sm-6">
-          <RegisterForm desktop={isMobile ? false : true} />
+          {
+            token
+              ? <Navigate to={'/'} replace />
+              : <RegisterForm desktop={isMobile ? false : true} />
+          }
         </div>
         <div className="col-sm-6 d-flex justify-content-center">
           <img src={isMobile ? logo : img} className="img-fluid" alt="imagen" />

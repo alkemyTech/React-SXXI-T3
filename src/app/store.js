940c658f02408140b56c71ca38fb93b2c5014702
GabrielVodopivec <vodopivecgabriel@gmail.com>
2022-12-02 14:13:25 -0300

    Correción de nombre en importacion del reducer de autenticación

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
-import authSlice from "../features/auth/authSlice";
+import authReducer from "../features/auth/authSlice";
 
 export default configureStore({
   reducer: {
-    auth: authSlice
+    auth: authReducer
   }
 });

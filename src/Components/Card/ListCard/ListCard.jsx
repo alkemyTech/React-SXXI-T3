ba480d6fb89e904788f9157aa240ac2968f29d7b
gperrz <gperez01.dev@gmail.com>
2022-12-02 12:53:30 -0300

    Agrego componente lazy image en list card

--- src/Components/Card/ListCard/ListCard.jsx
+++ src/Components/Card/ListCard/ListCard.jsx
@@ -1,19 +1,22 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import { LazyLoadImage } from 'react-lazy-load-image-component';
 
 import Button from "../../Button/Button";
 
+import 'react-lazy-load-image-component/src/effects/blur.css';
 import "./ListCard.css";
 
+
 export const ListCard = ({
-  id,
-  name,
-  content,
-  image,
-  buttonLabel,
-  variant,
-  linkTo,
-}) => {
+                           id,
+                           name,
+                           content,
+                           image,
+                           buttonLabel,
+                           variant,
+                           linkTo,
+                         }) => {
   const [isMobile, setIsMobile] = useState(false);
 
   useEffect(() => {
@@ -32,32 +35,37 @@ export const ListCard = ({
     if (isMobile) {
       return {
         __html:
-          content?.length <= 90 ? content : `${content?.substring(0, 90)}...`,
+            content?.length <= 90 ? content : `${content?.substring(0, 90)}...`,
       };
     }
     return {
       __html:
-        content?.length <= 185 ? content : `${content?.substring(0, 185)}...`,
+          content?.length <= 185 ? content : `${content?.substring(0, 185)}...`,
     };
   };
 
   return (
-    <div className={`list-card ${variant}`}>
-      <div className="list-card-image">
-        <img src={image} alt={name} />
-      </div>
-      <div className="list-card-content">
-        <div className="list-card-text" dangerouslySetInnerHTML={newsText()} />
-        <div className="list-card-btn-div">
-          <Link to={linkTo ? linkTo : `${id}`} className="list-card-link">
-            <Button
-              label={buttonLabel}
-              className="list-card-btn"
-              variant={variant}
-            />
-          </Link>
+      <div className={`list-card ${variant}`}>
+        <div className="list-card-image">
+          <LazyLoadImage
+              alt={name}
+              effect="blur"
+              wrapperClassName="lazy-img"
+              src={image}
+          />
+        </div>
+        <div className="list-card-content">
+          <div className="list-card-text" dangerouslySetInnerHTML={newsText()} />
+          <div className="list-card-btn-div">
+            <Link to={linkTo ? linkTo : `${id}`} className="list-card-link">
+              <Button
+                  label={buttonLabel}
+                  className="list-card-btn"
+                  variant={variant}
+              />
+            </Link>
+          </div>
         </div>
       </div>
-    </div>
   );
 };

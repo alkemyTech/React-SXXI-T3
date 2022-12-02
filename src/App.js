706fe0893ad4949d0829bcce481d486dbeefd013
GabrielVodopivec <vodopivecgabriel@gmail.com>
2022-12-02 10:49:45 -0300

    Agrego protección a ruta /backoffice

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import {Route, Routes} from 'react-router-dom';
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
 import Login from './Components/Auth/Login';
 import BackofficeRoutes from "./Routes/BackofficeRoutes";
 import Register from "./Components/Auth/Register";
@@ -14,33 +14,41 @@ import ActivitiesList from "./Components/Activities/ActivitiesList";
 import Home from "./Components/Home";
 import { Footer } from './Components/Footer/Footer';
 import { Header } from './Components/Header/Header';
+import ProtectedRoute from './Components/ProtectedRoute';
 
 import "./App.css";
 
-
 function App() {
-  const [isLogged, setIsLogged] = useState(false);
 
   return (
-      <Routes>
-        <Route path="backoffice/*" element={<BackofficeRoutes/>}/>
-        <Route path="login" element={<><Header/><Login/></>}/>
-        <Route path="register" element={<><Header/><Register/></>}/>
-        <Route path="actividades">
-          <Route index element={<><Header/><ActivitiesList/></>}/>
-          <Route path=":id" element={<><Header/><ActivitiesDetail/></>}/>
-        </Route>
-        <Route path="novedades">
-          <Route index element={<><Header/><NewsList/></>}/>
-          <Route path=":id" element={<><Header/><NewsDetail title="Novedad"/></>}/>
-        </Route>
-        <Route path="contacto" element={<><Header/><Contact/></>}/>
-        <Route path="nosotros" element={<><Header/><Nosotros/></>}/>
-        <Route path="donar" element={<><Header/><Donation/></>}/>
-        <Route path="gracias" element={<><Header/><Thanks/></>}/>
-        <Route path="/" element={<><Header isLogged={isLogged} handleLogged={setIsLogged}/><Home/><Footer/></>}/>
-        <Route path="*" element={<><Header/><Home/><Footer/></>}/>
-      </Routes>
+    <Routes>
+
+      <Route
+        path="backoffice/*"
+        element={
+          <ProtectedRoute>
+            <BackofficeRoutes />
+          </ProtectedRoute>
+        }
+      />
+
+      <Route path="login" element={<><Header /><Login /></>} />
+      <Route path="register" element={<><Header /><Register /></>} />
+      <Route path="actividades">
+        <Route index element={<><Header /><ActivitiesList /></>} />
+        <Route path=":id" element={<><Header /><ActivitiesDetail /></>} />
+      </Route>
+      <Route path="novedades">
+        <Route index element={<><Header /><NewsList /></>} />
+        <Route path=":id" element={<><Header /><NewsDetail title="Novedad" /></>} />
+      </Route>
+      <Route path="contacto" element={<><Header /><Contact /></>} />
+      <Route path="nosotros" element={<><Header /><Nosotros /></>} />
+      <Route path="donar" element={<><Header /><Donation /></>} />
+      <Route path="gracias" element={<><Header /><Thanks /></>} />
+      <Route path="/" element={<><Header /><Home /><Footer /></>} />
+      <Route path="*" element={<><Header /><Home /><Footer /></>} />
+    </Routes>
   );
 }
 

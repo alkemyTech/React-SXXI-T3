bbd914a154e27979e2b6c04b06cb36563d175dc2
naara cataldo <naarabelencataldo@outlook.com>
2022-12-04 11:13:06 -0300

    =comentarios en detalle novedades

--- src/Components/Comments/CommentsDetails.jsx
+++ src/Components/Comments/CommentsDetails.jsx
@@ -47,54 +47,57 @@ const CommentsDetails = ({ idNews }) => {
             </ul>
           </div>
         </div>
-        {commentsAux.length > 0 && (
-          <Stack spacing={1}>
-            <div className="container w-100 pb-5">
-              <div className="row mt-5 p-3">
-                <div className="col-sm-12">
-                  <div className="h-75" id="card">
-                    <Skeleton variant="circular" width={200} height={200} />
-                    <div className=" p-3 card m-2 bg-light" id="contenidoBody">
-                      <div className="  contenidoNombreFecha">
-                        <h5 className="h5 g-color-gray-dark-v1 mb-0">
-                          <strong>
+        {commentsAux.length > 0 &&
+          commentsAux.map((comentario, index) => {
+            return (
+              <Stack spacing={1} key={index}>
+              <div className="container w-100 pb-5">
+                <div className="row mt-5 p-3">
+                  <div className="col-sm-12">
+                    <div className="h-75" id="card">
+                      <Skeleton variant="circular" width={200} height={200} />
+                      <div className=" p-3 card m-2 bg-light" id="contenidoBody">
+                        <div className="  contenidoNombreFecha">
+                          <h5 className="h5 g-color-gray-dark-v1 mb-0">
+                            <strong>
+                              <Skeleton
+                                variant="text"
+                                sx={{ fontSize: "1rem" }}
+                                width={200}
+                                height={60}
+                              />
+                            </strong>
+                          </h5>
+                          <span className="text-muted ">
+                            <Skeleton
+                              variant="text"
+                              sx={{ fontSize: "1rem" }}
+                              width={100}
+                              height={60}
+                            />
+                          </span>
+                        </div>
+                        <div className="contenidoComentario">
+                          <p className="">
                             <Skeleton
                               variant="text"
                               sx={{ fontSize: "1rem" }}
                               width={200}
                               height={60}
                             />
-                          </strong>
-                        </h5>
-                        <span className="text-muted ">
-                          <Skeleton
-                            variant="text"
-                            sx={{ fontSize: "1rem" }}
-                            width={100}
-                            height={60}
-                          />
-                        </span>
-                      </div>
-                      <div className="contenidoComentario">
-                        <p className="">
-                          <Skeleton
-                            variant="text"
-                            sx={{ fontSize: "1rem" }}
-                            width={200}
-                            height={60}
-                          />
-                        </p>
-                        <button type="button" className="btn btn-primary">
-                          Responder
-                        </button>
+                          </p>
+                          <button type="button" className="btn btn-primary">
+                            Responder
+                          </button>
+                        </div>
                       </div>
                     </div>
                   </div>
                 </div>
               </div>
-            </div>
-          </Stack>
-        )}
+            </Stack>
+            );
+          })}
       </>
     );
   } else {

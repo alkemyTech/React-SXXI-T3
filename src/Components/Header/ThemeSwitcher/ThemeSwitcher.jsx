2889d0ff9843ba3ecaa095704ddbcd21b57f1037
GabrielVodopivec <vodopivecgabriel@gmail.com>
2022-12-03 22:33:13 -0300

    Modificacion en eventListeners de componente ThemeSwitcher

--- src/Components/Header/ThemeSwitcher/ThemeSwitcher.jsx
+++ src/Components/Header/ThemeSwitcher/ThemeSwitcher.jsx
@@ -1,24 +1,23 @@
-import {motion} from "framer-motion";
+import { motion } from "framer-motion";
 
-import {ReactComponent as SunSvg} from "../../../assets/svg/switch/sun.svg";
-import {ReactComponent as MoonSvg} from "../../../assets/svg/switch/moon.svg";
+import { ReactComponent as SunSvg } from "../../../assets/svg/switch/sun.svg";
+import { ReactComponent as MoonSvg } from "../../../assets/svg/switch/moon.svg";
 
 import './ThemeSwitcher.css';
 
 
-export const ThemeSwitcher = ({switchTheme, theme}) => {
+export const ThemeSwitcher = ({ switchTheme, theme }) => {
     return (
         <motion.button
             className="toggle-theme"
             onClick={switchTheme}
-            onTap={switchTheme}
-            whileTap={{scale: 0.8, rotate: 180}}
-            whileHover={{scale: 1.25}}
+            whileTap={{ scale: 0.8, rotate: 180 }}
+            whileHover={{ scale: 1.25 }}
         >
             {theme === 'dark' ?
-             <MoonSvg className="moon"/>
-                              :
-             <SunSvg className="moon"/>
+                <MoonSvg className="moon" />
+                :
+                <SunSvg className="moon" />
             }
         </motion.button>
     )

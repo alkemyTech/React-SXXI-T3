0d7a89541bb905f77b932f2c7a4365086ad56480
GabrielVodopivec <vodopivecgabriel@gmail.com>
2022-12-05 20:44:40 -0300

    Ahora el logo del Header de Backoffice es Link a la Web Pública

--- src/Components/Header/HeaderBackoffice/index.jsx
+++ src/Components/Header/HeaderBackoffice/index.jsx
@@ -1,7 +1,9 @@
 import s from "./headerBackoffice.module.css";
 import Sidebar from "../../Sidebar";
 import { useState } from "react";
+import { Link } from "react-router-dom";
 import { useLogo } from "../../../hooks/useLogo";
+import defaultLogo from "../../../assets/images/somosMasLogo.png"
 import HeaderSession from "../HeaderSession/HeaderSession";
 
 const HeaderBackoffice = ({ theme, switchTheme }) => {
@@ -22,13 +24,13 @@ const HeaderBackoffice = ({ theme, switchTheme }) => {
             <div className={s['header-backoffice']}>
                 <button className={s['sidebar-controler']} onClick={handleClick}>|||</button>
                 <HeaderSession showInfo={showInfo} handleShowInfo={handleShowInfo} theme={theme} switchTheme={switchTheme} />
-                <div className={s['logo-header']}>
+                <Link className={s['logo-header']} to='/'>
                     {
                         isFetching
                             ? null
-                            : <img src={logoONG} alt="logo" />
+                            : <img src={logoONG || defaultLogo} alt="logo" />
                     }
-                </div>
+                </Link>
             </div>
             <Sidebar active={showSidebar} handleClick={handleClick} />
         </>

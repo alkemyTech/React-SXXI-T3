9df5a8581b6819687409d477e7dbae81f2bf0e94
gperrz <116732317+gperrz@users.noreply.github.com>
2022-12-05 15:16:09 -0300

    Agrego componente lazy image en list card (#60)

--- src/Components/Card/ListCard/ListCard.jsx
+++ src/Components/Card/ListCard/ListCard.jsx
@@ -1,57 +1,73 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { LazyLoadImage } from 'react-lazy-load-image-component';
 
 import Button from "../../Button/Button";
 import { useMobile } from "../../../hooks/useIsMobile";
 
+import 'react-lazy-load-image-component/src/effects/blur.css';
 import "./ListCard.css";
 import { defaultImage } from "../../../utils/defaultImage";
 
+
 export const ListCard = ({
-  id,
-  name,
-  content,
-  image,
-  buttonLabel,
-  variant,
-  linkTo,
-}) => {
-  const isMobile = useMobile();
+                           id,
+                           name,
+                           content,
+                           image,
+                           buttonLabel,
+                           variant,
+                           linkTo,
+                         }) => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const updateDevice = () => {
+      setIsMobile(window.innerWidth <= 576);
+    };
+    updateDevice();
+    window.addEventListener("resize", updateDevice);
+
+    return () => {
+      window.removeEventListener("resize", updateDevice);
+    };
+  }, []);
 
   const newsText = () => {
     if (isMobile) {
       return {
         __html:
-          content?.length <= 90 ? content : `${content?.substring(0, 90)}...`,
+            content?.length <= 90 ? content : `${content?.substring(0, 90)}...`,
       };
     }
     return {
       __html:
-        content?.length <= 185 ? content : `${content?.substring(0, 185)}...`,
+          content?.length <= 185 ? content : `${content?.substring(0, 185)}...`,
     };
   };
 
   return (
-    <div className={`list-card ${variant}`}>
-      <div className="list-card-image">
-        <div className="list-card-divimg">
-          <img src={image || defaultImage} alt={name} />
-          <h6 className="list-card-title">{name}</h6>
+      <div className={`list-card ${variant}`}>
+        <div className="list-card-image">
+          <LazyLoadImage
+              alt={name}
+              effect="blur"
+              wrapperClassName="lazy-img"
+              src={image}
+          />
         </div>
-
-      </div>
-      <div className="list-card-content">
-        <div className="list-card-text" dangerouslySetInnerHTML={newsText()} />
-        <div className="list-card-btn-div">
-          <Link to={linkTo ? linkTo : `${id}`} className="list-card-link">
-            <Button
-              label={buttonLabel}
-              className="list-card-btn"
-              variant={variant}
-            />
-          </Link>
+        <div className="list-card-content">
+          <div className="list-card-text" dangerouslySetInnerHTML={newsText()} />
+          <div className="list-card-btn-div">
+            <Link to={linkTo ? linkTo : `${id}`} className="list-card-link">
+              <Button
+                  label={buttonLabel}
+                  className="list-card-btn"
+                  variant={variant}
+              />
+            </Link>
+          </div>
         </div>
       </div>
-    </div>
   );
 };

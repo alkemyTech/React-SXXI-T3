6833b1cf1edc96b774e4a6ec01f96d4b2fad37d3
Carlos Esposito <elcharlybeto@hotmail.com>
2022-12-13 12:38:12 -0300

    Redux para Novedades en Backofffice

--- src/Components/Backoffice/NewsList.jsx
+++ src/Components/Backoffice/NewsList.jsx
@@ -1,108 +1,111 @@
-import { useState } from 'react';
-import debounce from 'lodash.debounce';
+import debounce from "lodash.debounce";
+import { useEffect, useState } from "react";
 
-import Swal from 'sweetalert2';
+import Swal from "sweetalert2";
 
+import { apiNews } from "../../Services/apiService";
+import { errorAlert } from "../Feedback/AlertService";
 import BackofficeList from "./BackofficeList/BackofficeList";
-import { useBackofficeInfo } from './Hook';
-import { apiNews } from '../../Services/apiService';
-import { errorAlert } from '../Feedback/AlertService';
+
+import { useDispatch, useSelector } from "react-redux";
+import {
+	getBackofficeInfo, selectBackoffice
+} from "../../features/backoffice/backofficeSlice";
 
 export const NewsList = () => {
-	const [search, setSearch] = useState('');
-	const [selectedCategory, setSelectedCategory] = useState('');
-	const [info, isFetching, setRoute] = useBackofficeInfo('news');
+  const [search, setSearch] = useState("");
+  const [selectedCategory, setSelectedCategory] = useState("");
+  const { info, isFetching} = useSelector(selectBackoffice);
+  const dispatch = useDispatch();
 
-	const handleChange = debounce((event) => {
-		const { value } = event.target;
-		const cleanValue = value.trim();
+  useEffect(() => {
+    dispatch(getBackofficeInfo("news"));
+  }, [dispatch]);
 
-		setSearch(() => (cleanValue))
+  const handleChange = debounce((event) => {
+    const { value } = event.target;
+    const cleanValue = value.trim();
 
-		if (cleanValue.length >= 3) {
-			selectedCategory !== 'category=Todas las categorías'
-				? setRoute(`news?search=${cleanValue}&${selectedCategory}`)
-				: setRoute(`news?search=${cleanValue}`)
-		}
+    setSearch(() => cleanValue);
+    !cleanValue.length && dispatch(getBackofficeInfo("news"));
+    if (cleanValue.length >= 3) {
+      selectedCategory !== "category=Todas las categorías"
+        ? dispatch(getBackofficeInfo(`news?search=${cleanValue}&${selectedCategory}`))
+        : dispatch(getBackofficeInfo(`news?search=${cleanValue}`));
+    }
+  }, 1000);
 
-	}, 1000)
+  const handleSelectChange = (event) => {
+    const { value } = event.target;
+    setSelectedCategory(() => `category=${value}`);
+    if (search.length) {
+      value !== "Todas las categorías"
+        ? dispatch(getBackofficeInfo(() => `news?search=${search}&category=${value}`))
+        : dispatch(getBackofficeInfo(() => `news?search=${search}`));
+    } else {
+      value !== "Todas las categorías"
+        ? dispatch(getBackofficeInfo(() => `news?category=${value}`))
+        : dispatch(getBackofficeInfo(() => `news`));
+    }
+  };
 
-	const handleSelectChange = (event) => {
-		const { value } = event.target
-		setSelectedCategory(() => (`category=${value}`))
-		if (search.length) {
-			value !== 'Todas las categorías'
-				? setRoute(() => (`news?search=${search}&category=${value}`))
-				: setRoute(() => (`news?search=${search}`))
-		} else {
-			value !== 'Todas las categorías'
-				? setRoute(() => (`news?category=${value}`))
-				: setRoute(() => (`news`))
-		}
-	}
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (search.length) {
+      selectedCategory !== "category=Todas las categorías"
+        ? dispatch(getBackofficeInfo(() => `news?search=${search}&${selectedCategory}`))
+        : dispatch(getBackofficeInfo(() => `news?search=${search}`));
+    } else {
+      selectedCategory !== "category=Todas las categorías"
+        ? dispatch(getBackofficeInfo(() => `news?${selectedCategory}`))
+        : dispatch(getBackofficeInfo(() => `news`));
+    }
+  };
 
-	const handleSubmit = (event) => {
-		event.preventDefault();
-		if (search.length) {
-			selectedCategory !== 'category=Todas las categorías'
-				? setRoute(() => (`news?search=${search}&${selectedCategory}`))
-				: setRoute(() => (`news?search=${search}`))
-		} else {
-			selectedCategory !== 'category=Todas las categorías'
-				? setRoute(() => (`news?${selectedCategory}`))
-				: setRoute(() => (`news`))
-		}
-	}
+  const deleteNewHandler = (id) => {
+    Swal.fire({
+      title: `Se procederá a borrar la novedad ${id}`,
+      text: "Por favor, confirma",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Borrarla",
+      cancelButtonText: "Cancelar",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        apiNews
+          .remove(id)
+          .then((response) => {
+            Swal.fire("Novedad borrado!", "", "success");
+          })
+          .catch((error) => errorAlert());
+      }
+    });
+  };
 
-	const deleteNewHandler = (id) => {
-		Swal.fire({
-			title: `Se procederá a borrar la novedad ${id}`,
-			text: "Por favor, confirma",
-			icon: 'warning',
-			showCancelButton: true,
-			confirmButtonColor: '#3085d6',
-			cancelButtonColor: '#d33',
-			confirmButtonText: 'Borrarla',
-			cancelButtonText: "Cancelar"
-		}).then((result) => {
-			if (result.isConfirmed) {
-				apiNews.remove(id)
-				.then(response => {
-					Swal.fire(
-						'Novedad borrado!',
-						'',
-						'success'
-					)
-				})
-				.catch(error => errorAlert())
-			}
-		})
-	}
-
-	return (
-		<>
-			{
-				isFetching
-					? null
-					: <BackofficeList
-						deleteFunction={deleteNewHandler}
-						title="Novedades"
-						createButonLabel="novedad"
-						tableData={info}
-						tableHeader={["name", "image", "created_at"]}
-						tableNames={["Título", "Imagen", "Creado en"]}
-						handleChange={handleChange}
-						handleSubmit={handleSubmit}
-						handleSelectChange={handleSelectChange}
-						hasOptions={true}
-						placeholder={'Título de la novedad'}
-						source={{
-							route: 'categories',
-							externalResource: true,
-							resource: null
-						}}
-					/>
-			}
-		</>
-	)
-}
+  return (
+    <>
+      {isFetching ? null : (
+        <BackofficeList
+          deleteFunction={deleteNewHandler}
+          title="Novedades"
+          createButonLabel="novedad"
+          tableData={info}
+          tableHeader={["name", "image", "created_at"]}
+          tableNames={["Título", "Imagen", "Creado en"]}
+          handleChange={handleChange}
+          handleSubmit={handleSubmit}
+          handleSelectChange={handleSelectChange}
+          hasOptions={true}
+          placeholder={"Título de la novedad"}
+          source={{
+            route: "categories",
+            externalResource: true,
+            resource: null,
+          }}
+        />
+      )}
+    </>
+  );
+};
---
+++ src/features/backoffice/backofficeSlice.js
@@ -0,0 +1,65 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { errorAlert } from "../../Components/Feedback/AlertService";
+import { apiONG } from "../../Services/apiONG";
+
+export const getBackofficeInfo = createAsyncThunk(
+    'backofice/info',
+    async (route) => {
+        const { data } = await apiONG.get(route);
+        return data;
+    }
+)
+
+const initialState = {
+    isFetching: true,
+    info: []
+}
+
+export const backofficeSlice = createSlice({
+    name: 'backoffice',
+    initialState,
+    reducers: {
+        cleanInfo: (state) => ({ ...state, isFetching: true, info: [] })
+    },
+    extraReducers: (builder) => {
+        builder
+            .addCase(getBackofficeInfo.pending, (state) => {
+                state = { ...state, isFetching: true };
+            })
+            .addCase(getBackofficeInfo.fulfilled, (state, action) => {
+                const { payload: { error, data } } = action;
+                if (error) {
+                    const errorMessage =
+                        error?.response?.data?.message
+                        || error.message;
+
+                    return errorAlert(errorMessage);
+                }
+                return state = { isFetching: false, info: data }
+
+            })
+            .addCase(getBackofficeInfo.rejected, (state, action) => {
+                const { error } = action;
+
+                let errorMessage =
+                    error?.response?.data?.message
+                    || error.message;
+
+                errorMessage === 'Request failed with status code 404'
+                    ? errorMessage = 'Oops! Algo salió mal'
+                    : errorMessage = 'No tienes conexión a internet'
+
+                errorAlert(errorMessage);
+            })
+    }
+})
+
+export const selectBackoffice = (state) => state.backoffice;
+
+export const selectInfo = (state)=>state.backoffice.info;
+export const selectIsFetching = (state)=>state.backoffice.isFetching;
+
+
+export const { cleanInfo } = backofficeSlice.actions;
+
+export default backofficeSlice.reducer;
